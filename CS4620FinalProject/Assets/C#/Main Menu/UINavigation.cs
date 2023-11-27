16339ac698185dbdcb7124dce4e3783c6b6e2891
nl548218 <nl548218@ohio.edu>
2023-11-26 20:38:53 -0500

    Added the menu interaction

--- CS4620FinalProject/Assets/C#/Main Menu/UINavigation.cs
+++ CS4620FinalProject/Assets/C#/Main Menu/UINavigation.cs
@@ -8,12 +8,16 @@ public class UINavigation : MonoBehaviour
 {
     public Canvas StartingCanvas;
     public Canvas SelectionCanvas;
+    public Canvas NewGameCanvas;
+    public Canvas LoadSaveCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         StartingCanvas.enabled = true;
         SelectionCanvas.enabled = false;
+        NewGameCanvas.enabled = false;
+        LoadSaveCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -26,5 +30,24 @@ public class UINavigation : MonoBehaviour
     {
         StartingCanvas.enabled = false;
         SelectionCanvas.enabled = true;
+    }
+
+    public void ReturnBtn()
+    {
+        SelectionCanvas.enabled = true;
+        NewGameCanvas.enabled = false;
+        LoadSaveCanvas.enabled = false;
+    }
+
+    public void NewGameBtn()
+    {
+        SelectionCanvas.enabled = false;
+        NewGameCanvas.enabled = true;
+    }
+
+    public void LoadSaveBtn()
+    {
+        SelectionCanvas.enabled = false;
+        LoadSaveCanvas.enabled = true;
     }
 }
