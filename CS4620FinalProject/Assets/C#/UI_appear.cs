cb591de9afc895eb5832b874bd0b1f82c7ec1af8
Tarkis-Togulhorn <zt727820@ohio.edu>
2023-12-04 20:09:40 -0500

    pay bill

---
+++ CS4620FinalProject/Assets/C#/UI_appear.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class UI_appear : MonoBehaviour
+{
+
+
+[SerializeField] private Image customImage; 
+[SerializeField] private TextMeshProUGUI textDisplay; 
+[SerializeField] private Image customImage1; 
+void start(){
+customImage.enabled = false; 
+}
+
+void OnTriggerEnter(Collider other){
+
+    if(other.CompareTag("Player")){
+        customImage.enabled = true; 
+        customImage1.enabled = true; 
+        textDisplay.enabled = true;
+    }
+}
+
+void OnTriggerExit(Collider other){
+    if(other.CompareTag("Player")){
+        customImage.enabled = false;
+        customImage1.enabled = false; 
+        textDisplay.enabled = false;
+    }
+}
+}
